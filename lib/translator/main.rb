d99920f4ed75bf0da6c07f04b27b662035061f95
Patrick Salij <patrick-salij@hotmail.com>
2017-09-18 13:28:50 +0200

    Add options to remove some elements from translation E.g translate button

--- lib/translator/main.rb
+++ lib/translator/main.rb
@@ -9,7 +9,9 @@ module Translator
     end
 
     def translate(options ={})
-      text = options[:text]
+      scrubber = TranslatableTextScrubber.new
+      text = Loofah.fragment(options[:text]).scrub!(scrubber).to_s
+
       from = options[:from]
       if options[:to].present?
         to = options[:to]
@@ -23,5 +25,25 @@ module Translator
 
       Translator.callback.call(text, from, to)
     end
+  end
+end
+
+class TranslatableTextScrubber < Rails::Html::PermitScrubber
+  def initialize
+    super
+    self.tags = %w( a b i strong em p param h1 h2 h3 h4 h5 h6 br hr ul ol li img figure iframe div span blockquote question embed font video source audio )
+  end
+
+  def ignored_elements
+    %w(translate-link translate-exclude)
+  end
+
+  def scrub_node(node)
+    node.remove
+  end
+
+  def allowed_node?(node)
+    !(node.attributes["class"]&.name == "class" &&
+    (node.attributes["class"].value.split(" ") & ignored_elements).any?)
   end
 end
